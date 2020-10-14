ef993efd465a6ed0b43fda93d6382b40933a3b9d
Santiago Caro Duque <santiagocaroduque@gmail.com>
2020-10-13 21:48:32 -0500

    Routes done but commented

--- app/src/main/java/com/example/taller2/MapsActivity.java
+++ app/src/main/java/com/example/taller2/MapsActivity.java
@@ -7,6 +7,7 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -39,8 +40,19 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DirectionsLeg;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.DirectionsStep;
+import com.google.maps.model.EncodedPolyline;
+
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -186,6 +198,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                                     Double longitude1 = searchMarker.getPosition().longitude;
                                     Double longitude2 = locationMarker.getPosition().longitude;
                                     Toast.makeText(MapsActivity.this, "La distancia es: " + distance(latitude1,longitude1,latitude2,longitude2)+" Km", Toast.LENGTH_SHORT).show();
+                                    //getRoute(latitude2,longitude2,latitude1,longitude1);
                                 }
                             } else {Toast.makeText(MapsActivity.this, "Dirección no encontrada", Toast.LENGTH_SHORT).show();}
                         }
@@ -213,9 +226,77 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Double longitude2 = locationMarker.getPosition().longitude;
                 Toast.makeText(MapsActivity.this, "La distancia es: " + distance(latitude1,longitude1,latitude2,longitude2)+" Km", Toast.LENGTH_SHORT).show();
 
+                //getRoute(latitude2,longitude2,latitude1,longitude1);
+
             }
         });
 
+    }
+
+    private void getRoute(Double latitude1,Double longitude1,Double latitude2,Double longitude2) {
+
+        String origen = latitude1.toString()+","+longitude1.toString();
+        String destino = latitude2.toString()+","+longitude2.toString();
+        List<LatLng> path = new ArrayList();
+
+
+        //Execute Directions API request
+        GeoApiContext context = new GeoApiContext();
+        context.setApiKey("AIzaSyBqRcTbIZik18YabgfWg8G-EoTRV-zaUsE");
+
+        DirectionsApiRequest req = DirectionsApi.getDirections(context, origen,destino);
+        try{
+            DirectionsResult res = req.await();
+            Log.i("DIRECCION",res.toString());
+
+            //Loop through legs and steps to get encoded polylines of each step
+            if (res.routes != null && res.routes.length > 0) {
+                DirectionsRoute route = res.routes[0];
+
+                if (route.legs !=null) {
+                    for(int i=0; i<route.legs.length; i++) {
+                        DirectionsLeg leg = route.legs[i];
+                        if (leg.steps != null) {
+                            for (int j=0; j<leg.steps.length;j++){
+                                DirectionsStep step = leg.steps[j];
+                                if (step.steps != null && step.steps.length >0) {
+                                    for (int k=0; k<step.steps.length;k++){
+                                        DirectionsStep step1 = step.steps[k];
+                                        EncodedPolyline points1 = step1.polyline;
+                                        if (points1 != null) {
+                                            //Decode polyline and add points to list of route coordinates
+                                            List<com.google.maps.model.LatLng> coords1 = points1.decodePath();
+                                            for (com.google.maps.model.LatLng coord1 : coords1) {
+                                                path.add(new LatLng(coord1.lat, coord1.lng));
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    EncodedPolyline points = step.polyline;
+                                    if (points != null) {
+                                        //Decode polyline and add points to list of route coordinates
+                                        List<com.google.maps.model.LatLng> coords = points.decodePath();
+                                        for (com.google.maps.model.LatLng coord : coords) {
+                                            path.add(new LatLng(coord.lat, coord.lng));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+            Log.i("DIRECCION",e.getMessage());
+        }
+
+        //Draw the polyline
+        if (path.size() > 0) {
+            PolylineOptions opts = new PolylineOptions().addAll(path).color(Color.BLUE).width(5);
+            mMap.addPolyline(opts);
+        }
     }
 
     private String geoCoderSearchLatLang(LatLng latLng) {
